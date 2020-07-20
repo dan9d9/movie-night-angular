116deaeccba094e10bab6fd7a1fe75e10d8f05e5
Daniel Dick <dan9d9@gmail.com>
2020-07-20 22:20:36 +0200

    clean up welcome-modal component

--- src/app/welcome-modal/welcome-modal.component.ts
+++ src/app/welcome-modal/welcome-modal.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+import { MovieItem } from '../user-container/movie-list/movie-item/movie-item.model';
+
 @Component({
   selector: 'app-welcome-modal',
   templateUrl: './welcome-modal.component.html',
   styleUrls: ['./welcome-modal.component.css'],
 })
 export class WelcomeModalComponent implements OnInit {
-  @Output() user1: string;
-  @Output() user1Array: [] = [];
-  @Output() user2: string;
-  @Output() user2Array: [] = [];
   @Output() sendUserData: EventEmitter<{
     user1: string;
-    user1Array: [];
+    user1Array: MovieItem[];
     user2: string;
-    user2Array: [];
+    user2Array: MovieItem[];
   }> = new EventEmitter();
+
+  user1: string;
+  user1Array: MovieItem[] = [];
+  user2: string;
+  user2Array: MovieItem[] = [];
 
   handleSubmit(event) {
     event.preventDefault();
-    if (!this.user1.trim() || !this.user2.trim()) return;
 
-    this.getMovieLists(this.user1.trim(), this.user2.trim());
+    this.user1 = this.user1.trim();
+    this.user2 = this.user2.trim();
+    if (!this.user1 || !this.user2) return;
+
+    this.getMovieLists(this.user1, this.user2);
     this.sendUserData.emit({
-      user1: this.user1.trim(),
+      user1: this.user1,
       user1Array: this.user1Array,
-      user2: this.user2.trim(),
+      user2: this.user2,
       user2Array: this.user2Array,
     });
   }
